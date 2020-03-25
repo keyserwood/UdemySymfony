3c027fc573214c6166ed7200659d894e556a6ff8
keyserwood <elouan.raymond@etu.emse.fr>
2020-03-25 21:51:00 +0100

    adding famille feature

---
+++ Project2/src/Controller/FamilleController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\FamilleRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class FamilleController extends AbstractController
+{
+    /**
+     * @Route("/familles", name="familles")
+     */
+   public function index(FamilleRepository $repository)
+    {
+        $familles = $repository->findAll();
+        return $this->render('famille/index.html.twig', [
+            'familles' => $familles,
+        ]);
+    }
+    
+}
--- Project2/src/DataFixtures/AnimalFixtures.php
+++ Project2/src/DataFixtures/AnimalFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\Animal;
+use App\Entity\Famille;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
@@ -10,52 +11,73 @@ class AnimalFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-    $a1 = new Animal();
-    $a1->setNom("Chien")
-        ->setDescription("Un animal domestique")
-        ->setImage("chien.png")
-        ->setPoids(10)
-        ->setDangereux(false)
+        $c1 = new Famille();
+        $c1->setLibelle("mammifères")
+            ->setDescription("Animaux vertébrés nourrissant leurs petits avec du lait");
+        $manager->persist($c1);
+        $c2 = new Famille();
+        $c2->setLibelle("reptiles")
+            ->setDescription("Animaux vertébrés qui rampent");
+        $manager->persist($c2);
+        $c3 = new Famille();
+        $c3->setLibelle("poissons")
+            ->setDescription("Animaux invertébrés du monde aquatique");
+        $manager->persist($c3);
+
+        $a1 = new Animal();
+        $a1->setNom("Chien")
+            ->setDescription("Un animal domestique")
+            ->setImage("chien.png")
+            ->setPoids(10)
+            ->setDangereux(false)
+            ->setFamille($c1)
+            ;
+        $manager -> persist($a1);
+        $a2 = new Animal();
+        $a2->setNom("Cochon")
+            ->setDescription("Un animal d'elevage")
+            ->setImage("cochon.png")
+            ->setPoids(50)
+            ->setDangereux(false)
+            ->setFamille($c1)
+
         ;
-    $manager -> persist($a1);
-    $a2 = new Animal();
-    $a2->setNom("Cochon")
-        ->setDescription("Un animal d'elevage")
-        ->setImage("cochon.png")
-        ->setPoids(50)
-        ->setDangereux(false)
-    ;
-    $manager -> persist($a2);
+        $manager -> persist($a2);
 
-    $a3 = new Animal();
-    $a3->setNom("Croco")
-        ->setDescription("Un animal sauvage")
-        ->setImage("croco.png")
-        ->setPoids(300)
-        ->setDangereux(true)
-    ;
-    $manager -> persist($a3);
+        $a3 = new Animal();
+        $a3->setNom("Croco")
+            ->setDescription("Un animal sauvage")
+            ->setImage("croco.png")
+            ->setPoids(300)
+            ->setDangereux(true)
+            ->setFamille($c2);
 
-    $a4 = new Animal();
-    $a4->setNom("Requin")
-        ->setDescription("Un animal marin")
-        ->setImage("requin.png")
-        ->setPoids(800)
-        ->setDangereux(true)
-    ;
-    $manager -> persist($a4);
+        $manager -> persist($a3);
 
-    $a5 = new Animal();
-    $a5->setNom("Serpent")
-        ->setDescription("Un animal imprévisible")
-        ->setImage("serpent.png")
-        ->setPoids(5)
-        ->setDangereux(true)
-    ;
-    $manager -> persist($a5);
-        // $product = new Product();
-        // $manager->persist($product);
+        $a4 = new Animal();
+        $a4->setNom("Requin")
+            ->setDescription("Un animal marin")
+            ->setImage("requin.png")
+            ->setPoids(800)
+            ->setDangereux(true)
+            ->setFamille($c3)
 
-    $manager->flush();
+        ;
+        $manager -> persist($a4);
+
+        $a5 = new Animal();
+        $a5->setNom("Serpent")
+            ->setDescription("Un animal imprévisible")
+            ->setImage("serpent.png")
+            ->setPoids(5)
+            ->setDangereux(true)
+            ->setFamille($c2)
+
+        ;
+        $manager -> persist($a5);
+            // $product = new Product();
+            // $manager->persist($product);
+
+        $manager->flush();
     }
 }
