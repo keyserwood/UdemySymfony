f5b0c99e60cba855516ef47cce3446f7ad2213c5
keyserwood <elouan.raymond@etu.emse.fr>
2020-03-28 11:47:31 +0100

    Modification and Add !!

--- Project3/src/Controller/Admin/AdminAlimentController.php
+++ Project3/src/Controller/Admin/AdminAlimentController.php
@@ -5,7 +5,10 @@ namespace App\Controller\Admin;
 use App\Entity\Aliment;
 use App\Form\AlimentType;
 use App\Repository\AlimentRepository;
+use Doctrine\Common\Persistence\ManagerRegistry;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AdminAlimentController extends AbstractController
@@ -21,14 +24,31 @@ class AdminAlimentController extends AbstractController
         ]);
     }
     /**
+     * @Route("/admin/aliment/creation", name="admin_aliment_creation")
      * @Route("/admin/aliment/{id}", name="admin_aliment_modification")
      */
-    public function modification(Aliment $aliment)
+    public function ajoutetModif(Aliment $aliment=null, Request $request, ManagerRegistry $managerRegistry)
+
+
     {
+        if (!$aliment)
+        {
+            $aliment= new Aliment();
+        }
         $form = $this->createForm(AlimentType::class,$aliment);
-        return $this->render('admin/admin_aliment/modificationAliment.html.twig', [
+
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $em = $managerRegistry->getManager();
+            $em->persist($aliment);
+            $em->flush();
+            return $this->redirectToRoute("admin_aliments");
+        }
+        return $this->render('admin/admin_aliment/modificationEtAjoutAliment.html.twig', [
             'aliment' => $aliment,
-            "form"=>$form->createView()
+            "form"=>$form->createView(),
+            "isModification"=> $aliment->getId() !== null
         ]);
     }
 }
