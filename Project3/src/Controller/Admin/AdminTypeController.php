2a397d162602087af77f4254d9c2df76536cc355
keyserwood <elouan.raymond@etu.emse.fr>
2020-03-31 22:31:21 +0200

    Change & Delete OK for types

--- Project3/src/Controller/Admin/AdminTypeController.php
+++ Project3/src/Controller/Admin/AdminTypeController.php
@@ -2,8 +2,13 @@
 
 namespace App\Controller\Admin;
 
+
+use App\Entity\Type;
+use App\Form\TypesType;
 use App\Repository\TypeRepository;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AdminTypeController extends AbstractController
@@ -18,4 +23,51 @@ class AdminTypeController extends AbstractController
             'types' => $types
         ]);
     }
+
+    /**
+     * @Route("/admin/types/creation", name="admin_type_creation")
+     * @Route("/admin/types/{id}", name="admin_type_modification", methods="POST")
+     */
+    public function ajoutEtModif(Type $type = null, Request $request,  ManagerRegistry $managerRegistry)
+    {
+        if(!$type) {
+            $type = new Type();
+        }
+
+        $form = $this->createForm(TypesType::class,$type);
+
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $modif = $type->getId() !== null;
+            $em = $managerRegistry->getManager();
+            $em->persist($type);
+            $em->flush();
+            $this->addFlash("success", ($modif)? "La modification a été effectuée" : "L'ajout a été effectué");
+            return $this->redirectToRoute("admin_types");
+        }
+
+        return $this->render('admin/admin_type/modifAjoutType.html.twig',[
+            "type" => $type,
+            "form" => $form->createView(),
+            "isModification" => $type->getId() !== null
+        ]);
+    }
+
+    /**
+     * @Route("/admin/types/{id}", name="admin_type_suppression", methods="delete")
+     */
+    public function suppression(Type $type, Request $request,  ManagerRegistry $managerRegistry){
+        if($this->isCsrfTokenValid("SUP".$aliment->getId(),$request->get('_token'))){
+
+            $em = $managerRegistry->getManager();
+            $em->remove($aliment);
+            $em->flush();
+            $this->addFlash("success","La suppression a été effectuée");
+            return $this->redirectToRoute("admin_types");
+
+
+        }
+
+    }
+
 }
