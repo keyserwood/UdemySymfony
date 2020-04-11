6f0cb677e5a5004a7af543ab0266d8ee4793031e
keyserwood <elouan.raymond@etu.emse.fr>
2020-04-11 10:11:29 +0200

    Update User

--- Project4/src/Controller/GlobalController.php
+++ Project4/src/Controller/GlobalController.php
@@ -2,8 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\RechercheVoiture;
+use App\Entity\Utilisateur;
+use App\Form\InscriptionType;
 use App\Repository\VoitureRepository;
+use Doctrine\Common\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class GlobalController extends AbstractController
@@ -15,6 +20,27 @@ class GlobalController extends AbstractController
     {
         return $this->render('global/index.html.twig', [
             'controller_name' => 'GlobalController',
+        ]);
+    }
+    /**
+     * @Route("/inscription", name="inscription")
+     */
+    public function inscription(Request $request,ManagerRegistry $managerRegistry)
+    {
+        $utilisateur =new Utilisateur();
+        $form = $this->createForm(InscriptionType::class,$utilisateur);
+        $form->handleRequest($request);
+        if($form->isSubmitted()&&$form->isValid())
+        {
+            $em = $managerRegistry->getManager();
+            $utilisateur->setRoles("ROLE_USER");
+            $em->persist($utilisateur);
+            $em->flush();
+            return $this->redirectToRoute("accueil");
+
+        }
+        return $this->render('global/inscription.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 
---
+++ Project4/src/Entity/Utilisateur.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\UtilisateurRepository")
+ */
+class Utilisateur
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $username;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $password;
+    private $verifPassword;
+
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $roles;
+
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+
+    public function getVerifPassword(): ?string
+    {
+        return $this->verifPassword;
+    }
+
+    public function setVerifPassword(string $verifPassword): self
+    {
+        $this->verifPassword = $verifPassword;
+
+        return $this;
+    }
+
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUsername(): ?string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(string $username): self
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+
+    public function getRoles(): ?string
+    {
+        return $this->roles;
+    }
+
+    public function setRoles(string $roles): self
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+}
